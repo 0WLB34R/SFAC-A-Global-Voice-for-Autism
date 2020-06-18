2b287f0580b4f7df1f63e1a28c20e0cabcbc39b2
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2020-06-17 22:33:49 -0400

    Commit_splash_activity_intent_next_activity_icon1

--- app/src/main/java/com/sfac/AGlobalVoiceForAutism/SplashActivity.java
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/SplashActivity.java
@@ -50,7 +50,7 @@ public class SplashActivity extends AppCompatActivity {
                     Thread.sleep(300);
                     updateProgress(60);
 
-                    Thread.sleep(300);
+                    Thread.sleep(250);
                     updateProgress(70);
 
                     Thread.sleep(1100);
