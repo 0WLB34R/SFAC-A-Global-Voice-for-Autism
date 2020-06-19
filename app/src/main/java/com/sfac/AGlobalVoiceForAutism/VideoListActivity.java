c759fdfedcc4065e66a13287490a38fb15fb2240
Manu200162 <manli200134@gmail.com>
2020-06-19 00:40:53 -0400

    buttons_video_list

--- app/src/main/java/com/sfac/AGlobalVoiceForAutism/VideoListActivity.java
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/VideoListActivity.java
@@ -2,6 +2,7 @@ package com.sfac.AGlobalVoiceForAutism;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.GridLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sfac.AGlobalVoiceForAutism.adapter.ActivitiesRecyclerViewAdapter;
+import com.sfac.AGlobalVoiceForAutism.callBack.PlayVideoCallBack;
 import com.sfac.AGlobalVoiceForAutism.model.ActivitiesItem;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ import java.util.List;
 public class VideoListActivity extends AppCompatActivity {
      private Context context;
      private RecyclerView taskRecyclerView;
+     private ActivitiesRecyclerViewAdapter adapter;
      private List<ActivitiesItem> items = new ArrayList<>();
 
     @Override
@@ -31,21 +34,32 @@ public class VideoListActivity extends AppCompatActivity {
      fillvideoList();
  }
     private void initviews(){
-      taskRecyclerView = findViewById(R.id.taskRecyclerView);
-        ActivitiesRecyclerViewAdapter adapter = new ActivitiesRecyclerViewAdapter(context, items);
+        taskRecyclerView = findViewById(R.id.taskRecyclerView);
+        adapter = new ActivitiesRecyclerViewAdapter(context, items);
         taskRecyclerView.setAdapter(adapter);
         taskRecyclerView.setLayoutManager(new LinearLayoutManager(context));
     }
     private void addEvents(){
-
+       adapter.setCallBack(new PlayVideoCallBack() {
+           @Override
+           public void onTaskClicked(ActivitiesItem task) {
+              Log.e("OnTaskClicked", task.getLessons()+"Play_video");
+           }
+       });
+       adapter.setCallBack(new PlayVideoCallBack() {
+           @Override
+           public void onTaskClicked(ActivitiesItem task) {
+              Log.e("OnTaskClicked",task.getLessons()+"Quiz");
+           }
+       });
     }
     private void fillvideoList(){
        items.add(new ActivitiesItem(1,"Lesson #1",R.drawable.activities));
-       items.add(new ActivitiesItem(1,"Lesson #2",R.drawable.worksheet));
-       items.add(new ActivitiesItem(1,"Lesson #1",R.drawable.activities));
-       items.add(new ActivitiesItem(1,"Lesson #2",R.drawable.worksheet));
-       items.add(new ActivitiesItem(1,"Lesson #1",R.drawable.activities));
-       items.add(new ActivitiesItem(1,"Lesson #2",R.drawable.worksheet));
+       items.add(new ActivitiesItem(2,"Lesson #2",R.drawable.worksheet));
+       items.add(new ActivitiesItem(3,"Lesson #3",R.drawable.activities));
+       items.add(new ActivitiesItem(4,"Lesson #4",R.drawable.worksheet));
+       items.add(new ActivitiesItem(5,"Lesson #5",R.drawable.activities));
+       items.add(new ActivitiesItem(6,"Lesson #6",R.drawable.worksheet));
 
     }
 }
---
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/callBack/PlayVideoCallBack.java
@@ -0,0 +1,6 @@
+package com.sfac.AGlobalVoiceForAutism.callBack;
+import com.sfac.AGlobalVoiceForAutism.model.ActivitiesItem;
+
+public interface PlayVideoCallBack {
+  void onTaskClicked(ActivitiesItem task);
+}
