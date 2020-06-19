569599d2ade072a1c520f0c5d6874ca33728feba
0WLB34R <knhrvs@gmail.com>
2020-06-18 21:21:39 -0400

    Ken Hervas: -Added Video Activity and Corresponding Layout -Added YouTube API -Added ability to view videos based on Keys

--- app/src/main/java/com/sfac/AGlobalVoiceForAutism/MainActivity.java
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoCommunity(View view){
-        Intent intent = new Intent(this, CommunityActivity.class);
+        Intent intent = new Intent(this,VideoActivity.class);
         startActivity(intent);
 
     }
---
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/VideoActivity.java
@@ -0,0 +1,104 @@
+package com.sfac.AGlobalVoiceForAutism;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+
+public class VideoActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.PlaybackEventListener, YouTubePlayer.PlayerStateChangeListener{
+
+    YouTubePlayerView playerView;
+    String videoId = "4MKAf6YX_7M";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        playerView = (YouTubePlayerView)findViewById(R.id.ytplayerView);
+        playerView.initialize(YouTubeConfig.getApiKey(),this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.setPlayerStateChangeListener(this);
+        youTubePlayer.setPlaybackEventListener(this);
+
+        if(!b){
+            youTubePlayer.cueVideo(videoId);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
+    @Override
+    public void onPlaying() {
+
+    }
+
+    @Override
+    public void onPaused() {
+
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+
+    }
+
+    @Override
+    public void onSeekTo(int i) {
+
+    }
+
+    @Override
+    public void onLoading() {
+
+    }
+
+    @Override
+    public void onLoaded(String s) {
+
+    }
+
+    @Override
+    public void onAdStarted() {
+
+    }
+
+    @Override
+    public void onVideoStarted() {
+
+    }
+
+    @Override
+    public void onVideoEnded() {
+
+    }
+
+    @Override
+    public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+    }
+
+    public void gotoQuiz(View view){
+        Intent intent = new Intent(this,CommunityActivity.class);
+        startActivity(intent);
+
+    }
+}
