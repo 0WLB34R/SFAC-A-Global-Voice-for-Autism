19da90c5cfa7aa743475836ba4e25708792ece90
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2020-06-18 23:51:31 -0400

    Commit_result_main_activity_button_funciton_and_library_implementations

--- app/src/main/java/com/sfac/AGlobalVoiceForAutism/MainActivity.java
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/MainActivity.java
@@ -5,24 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton button;
+    private ImageButton button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_layout);
+        button = findViewById(R.id.education_button);
+        button2 = findViewById(R.id.community);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCommunityActivity();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openVideoListActivity();
+            }
+        });
     }
-
-    public void gotoCommunity(View view){
-        Intent intent = new Intent(this,VideoActivity.class);
-        startActivity(intent);
-
-    }
-    public void gotoEducation(View view){
+    public void openVideoListActivity(){
         Intent intent = new Intent(this, VideoListActivity.class);
         startActivity(intent);
-
+    }
+    public void openCommunityActivity(){
+        Intent intent = new Intent(this, CommunityActivity.class);
+        startActivity(intent);
     }
 
 }
