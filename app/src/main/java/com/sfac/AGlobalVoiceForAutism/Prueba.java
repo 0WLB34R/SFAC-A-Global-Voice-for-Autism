53850ce18708fe1a8934e9259e8f928b695105fc
Manu200162 <manli200134@gmail.com>
2020-06-20 15:26:15 -0400

    card_view_RecycleView

--- app/src/main/java/com/sfac/AGlobalVoiceForAutism/Prueba.java
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/Prueba.java
@@ -20,19 +20,12 @@ public class Prueba extends AppCompatActivity {
     }
     private void receiveValues() {
         Intent intent = getIntent();
-
-        //2. Verificamos que el canal tenga el mensaje con la clave: "objUser"
         if (intent.hasExtra(Constants.INTENT_KEY_ACTIVITY)) {
-
-            //3. Obtener el valor del mensaje con la clave: "objUser"
             String userObj = intent.getStringExtra(Constants.INTENT_KEY_ACTIVITY);
-
-            //4. Convertir el String a un Objeto
             ActivitiesItem aI = new Gson().fromJson(userObj, ActivitiesItem.class);
-
-            Toast.makeText(Prueba.this, //Origen
-                    "Bienvenid@: "+ aI.getId() + aI.getExtention()+aI.getName() , //Mensaje
-                    Toast.LENGTH_SHORT) //Duracion
+            Toast.makeText(Prueba.this,
+                    "Bienvenid@: "+ aI.getId() + aI.getExtention()+aI.getName() ,
+                    Toast.LENGTH_SHORT)
                     .show();
         }
     }
