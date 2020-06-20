79d76a880fa1145021ee35f4c87e48e3da3d6007
0WLB34R <knhrvs@gmail.com>
2020-06-20 17:57:02 -0400

    Ken Hervas (0WLB34R): -Color Corrections

--- app/src/main/java/com/sfac/AGlobalVoiceForAutism/CommunityActivity.java
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/CommunityActivity.java
@@ -1,5 +1,7 @@
 package com.sfac.AGlobalVoiceForAutism;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ public class CommunityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#8BC34A"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.com_section);
     }
