5a3682dc5fb8e5e97aea5beee17486bd68ac84f0
Rodrivilas <rodriviladegut@gmail.com>
2020-06-30 19:07:19 -0400

    Whatsapp group list converted to recyclerview and proper groups added for various groups

---
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/adapter/WhatsappViewHolder.java
@@ -0,0 +1,20 @@
+package com.sfac.AGlobalVoiceForAutism.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sfac.AGlobalVoiceForAutism.R;
+
+public class WhatsappViewHolder extends RecyclerView.ViewHolder{
+    public TextView title;
+    public TextView description;
+
+    public WhatsappViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.title = itemView.findViewById(R.id.titleGroup);
+        this.description = itemView.findViewById(R.id.decriptionGroup);
+    }
+}
---
+++ app/src/main/java/com/sfac/AGlobalVoiceForAutism/callBack/WhatsappCallBack.java
@@ -0,0 +1,7 @@
+package com.sfac.AGlobalVoiceForAutism.callBack;
+
+import com.sfac.AGlobalVoiceForAutism.model.Whatsapp;
+
+public interface WhatsappCallBack {
+    void onGroupClicked(Whatsapp task);
+}
